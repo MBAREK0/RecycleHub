338dbb66462043845a88c3ceded74104f74e2314
MBAREK0 <elaadraouimbarek2023@gmail.com>
2025-02-06 23:09:44 +0100

    added the guards

---
+++ src/core/guards/auth.guard.ts
@@ -0,0 +1,36 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import {UserService} from "../service/user.service";
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const userService = inject(UserService);
+
+  // Get token from storage
+  const userId = localStorage.getItem('userId');
+
+  if (!userId) {
+    router.navigate(['/login']).then(r => console.log(r));
+    return false;
+  }
+
+  // Decode token
+
+  const requiredRoles: string[] = route.data['role'] || [];
+
+  const userRole = localStorage.getItem('userRole');
+
+  if (!userRole) {
+    router.navigate(['/auth/login']);
+    return false;
+  }
+
+  // Check roles
+  if (requiredRoles.length > 0 && !requiredRoles.includes(userRole)) {
+    router.navigate(['/login']);
+    return false;
+  }
+
+
+  return true;
+};
